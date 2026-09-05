545c890eee8ed130e58cfa885987a3821634843b
kevinmfwakley23-ux <kevinmfwakley23@gmail.com>
2026-09-05 00:51:02 -0600

    style(vault): load Curator tag recommendation styles

--- apps/web/public/vault-ui-styles.js
+++ apps/web/public/vault-ui-styles.js
@@ -8,6 +8,7 @@ const ENRICHMENT_STYLES = Object.freeze([
   "/vault-system-views.css",
   "/vault-sets.css",
   "/vault-marketplace-readiness.css",
+  "/vault-tag-recommendations.css",
   "/vault-accessibility.css"
 ]);
 
