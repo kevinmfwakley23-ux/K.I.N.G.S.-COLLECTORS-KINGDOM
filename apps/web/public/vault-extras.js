744d001eeb593f47622ff54abb3b08e85fd7529e
kevinmfwakley23-ux <kevinmfwakley23@gmail.com>
2026-09-05 10:33:21 -0600

    IMP-005: load autograph comparison after grading lab

--- apps/web/public/vault-extras.js
+++ apps/web/public/vault-extras.js
@@ -7,7 +7,8 @@ export const VAULT_EXTRA_MODULES = Object.freeze([
   "./vault-bulk-reorganization-ui.js",
   "./vault-saved-views-ui.js",
   "./vault-grading-ui.js",
-  "./vault-grading-color-ui.js"
+  "./vault-grading-color-ui.js",
+  "./vault-grading-autograph-ui.js"
 ]);
 
 export async function loadVaultExtras(loadModule = (specifier) => import(specifier)) {
