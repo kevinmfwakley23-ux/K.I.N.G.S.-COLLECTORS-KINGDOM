5e3cd0277f3d73371570365cd144ea52dea019ce
kevinmfwakley23-ux <kevinmfwakley23@gmail.com>
2026-09-05 05:29:22 -0600

    IMP-005: add ordered Vault enhancement bootstrap

---
+++ apps/web/public/vault-extras.js
@@ -0,0 +1,16 @@
+export const VAULT_EXTRA_MODULES = Object.freeze([
+  "./vault-import-ui.js",
+  "./vault-intake-ui.js",
+  "./vault-scanner-ui.js",
+  "./vault-provenance-ui.js"
+]);
+
+export async function loadVaultExtras(loadModule = (specifier) => import(specifier)) {
+  if (typeof loadModule !== "function") throw new TypeError("Vault extras loader must be a function.");
+  const loaded = [];
+  for (const specifier of VAULT_EXTRA_MODULES) {
+    await loadModule(specifier);
+    loaded.push(specifier);
+  }
+  return Object.freeze(loaded);
+}
